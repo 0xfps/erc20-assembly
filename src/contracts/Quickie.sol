4ec5fcd97637386cec756340633dc03d6c95df2d
tanim0la <izukfred@gmail.com>
2023-03-24 12:06:27 +0100

    fixes

--- src/contracts/Quickie.sol
+++ src/contracts/Quickie.sol
@@ -5,23 +5,23 @@ contract W {
     function _name() public pure returns (string memory) {
         // Return the name of the contract.
         assembly {
-            mstore(0x20, 0x20)
-            mstore(0x47, 0x07536561706f7274)
-            return(0x20, 0x60)
+            mstore(0x00, 0x20)
+            mstore(0x27, 0x07536561706f7274)
+            return(0x00, 0x60)
         }
     }
 
-    function hash() public pure returns(bytes memory) {
+    function hash() public pure returns (bytes memory) {
         return bytes("$STKN");
         // 0x536561706f7274
     }
 
-    function read() public pure returns(string memory) {
+    function read() public pure returns (string memory) {
         assembly {
-            mstore(0x20, 0x20) // Why store 0x20 at 0x20?
-            mstore(0x40, 0x07) // Length
-            mstore(0x60, shl(mul(0x19, 8), 0x536561706f7274)) // Actual string, set at the start.
-            return(0x20, 0x60) // Read from 0x20 and stop at 0x80.
+            mstore(0x00, 0x20) // stores the string offset in memory location `0x00`
+            mstore(0x20, 0x07) // Length
+            mstore(0x40, shl(0xc8, 0x536561706f7274)) // Actual string, set at the start.
+            return(0x00, 0x60) // Read from 0x00 to 0x00+0x60.
         }
     }
 }
